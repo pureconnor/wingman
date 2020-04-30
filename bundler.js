d2d6e30b70478f8bf8e357326b86a313a3aa768c
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-30 16:59:06 -0400

    use babylon to view tree in ast

--- bundler.js
+++ bundler.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const babylon = require('babylon')
 /*
     This file is used for reading the contents of the file
     using a native node module 'fs'
@@ -8,7 +9,11 @@ function createAsset(filename){
     // Reads contents of file as a string
     const content = fs.readFileSync(filename, 'utf-8')
 
-    console.log(content)
+    const ast = babylon.parse(content, {
+        sourceType: 'module'
+    });
+    
+    console.log(ast)
 }
 
 createAsset('./example/entry.js');
