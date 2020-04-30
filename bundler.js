c79e9a55740abaa795e8a2379051dbdc4a70ae77
Lacy, Connor <lacyconnor1409@gmail.com>
2020-04-30 16:37:04 -0400

    bundler start

---
+++ bundler.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+/*
+    This file is used for reading the contents of the file
+    using a native node module 'fs'
+*/
+
+function createAsset(filename){
+    // Reads contents of file as a string
+    const content = fs.readFileSync(filename, '')
+
+    console.log(content)
+}
+
+createAsset('./example/entry.js');
